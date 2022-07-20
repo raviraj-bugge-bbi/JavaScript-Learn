85967e589e44c9ece1d1dde254d39b75bcd6f866
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-20 12:23:44 +0530

    done fundamental part - 1

---
+++ fundamentals_part_1_coding_challenge_4.js
@@ -0,0 +1,7 @@
+let bill = 275;
+
+let tip = (bill >= 50 && bill <= 300) ? bill * 15 / 100 : bill * 20 / 100;
+
+let totalBill = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${totalBill}`);
