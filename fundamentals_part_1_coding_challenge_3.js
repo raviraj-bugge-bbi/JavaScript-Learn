97262da2a0bcf7dac52045bec6afc38cbf874f0a
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-20 11:13:48 +0530

    done challengr 3

---
+++ fundamentals_part_1_coding_challenge_3.js
@@ -0,0 +1,12 @@
+const dolphineScore = (96 + 108 + 89) / 3;
+const koalaScore = (88 + 91 + 110) / 3;
+
+console.log("Average score of team dolphines : " + dolphineScore);
+console.log("Average score of team koalas : " + koalaScore);
+
+if(dolphineScore > koalaScore){
+    console.log("Team dolphine wins the trophy!");
+}
+else{
+    console.log("Team koala wins the trophy!");
+}
