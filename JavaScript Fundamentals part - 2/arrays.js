f633123df9d3e13931090c17a4cf60fe8e4b0423
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-21 16:40:35 +0530

    array operations

---
+++ JavaScript Fundamentals part - 2/arrays.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+console.log("---LECTURE: Introduction to Arrays---");
+
+let populations = [33,55,14,17];
+
+if(populations.length == 4){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+let percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]),percentageOfWorld1(populations[2]),percentageOfWorld1(populations[3]),];
+console.log(percentages);
+
