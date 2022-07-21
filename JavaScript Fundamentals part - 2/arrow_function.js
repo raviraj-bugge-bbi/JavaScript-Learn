f2de977f4102e90f7827d43d88470ed8f2985ce1
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-21 12:19:27 +0530

    .

--- JavaScript Fundamentals part - 2/arrow_function.js
+++ JavaScript Fundamentals part - 2/arrow_function.js
@@ -11,3 +11,14 @@ console.log(italyPopulation);
 
 roamPopulation = percentageOfWorld1(130);
 console.log(roamPopulation);
+
+//----------------------------------------------------------------------------------------------------
+
+let retirementAge = birtYear => {
+    let age = 2022 - birtYear;
+    let retirement = 35-age;
+    return age, retirement;
+}
+
+let yearsLeft = retirementAge(1999);
+console.log(yearsLeft);
