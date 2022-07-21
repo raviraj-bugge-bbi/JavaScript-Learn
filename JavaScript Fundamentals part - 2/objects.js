a96be8bc26e99faaedb5139833ce297373382457
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-21 17:41:54 +0530

    started objects

---
+++ JavaScript Fundamentals part - 2/objects.js
@@ -0,0 +1,7 @@
+`use strict`;
+
+console.log("---LECTURE: Introduction to Objects---");
+
+let myCountry = {country:'india', capital:'delhi', population:'128', neighbours:['japan', 'russia']};
+
+console.log(myCountry);
