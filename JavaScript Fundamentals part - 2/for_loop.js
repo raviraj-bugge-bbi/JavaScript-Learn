ade2192f421e88badfb38d5bd6a900ce65aea56b
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-22 18:28:30 +0530

    done for loop

---
+++ JavaScript Fundamentals part - 2/for_loop.js
@@ -0,0 +1,8 @@
+`use strict`;
+
+console.log("----LECTURE: Iteration: The for Loop----");
+//---------------------------------------------------------------------------------
+
+for(let vote = 1; vote <= 50; vote++){
+    console.log(`Voter number ${vote} is currently voting`);
+}
