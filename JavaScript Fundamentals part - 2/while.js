fdc8d85ab99969149bec178a4a088df8ffd1096f
BBIM1027 <bbi@BBIs-MacBook-Pro.local>
2022-07-25 16:20:46 +0530

    while loop

---
+++ JavaScript Fundamentals part - 2/while.js
@@ -0,0 +1,14 @@
+`use strict`;
+
+console.log("----while loop----");
+
+let i = 0;
+let percentages4 = [];
+
+while(i<percentages3.length){
+    let perc = percentageOfWorld1(percentages3[i]);
+    percentages4.push(perc);
+    i++;
+}
+
+console.log(percentages4);
