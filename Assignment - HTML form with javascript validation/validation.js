527614ac934e28884c6eb552b3b5c505537e950e
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-07-28 17:11:24 +0530

    completed Assignment html form with validation and json #1

--- Assignment - HTML form with javascript validation/validation.js
+++ Assignment - HTML form with javascript validation/validation.js
@@ -1,10 +1,10 @@
 `use strict`;
 
 var regexNotNumAndChar = /[0-9!@#\  $%\^\&*\)\(+=._-]/;
-var regexContact = /[+][9][1]\s[7-9][0-9]{4}\s[0-9]{5}/g;
+var regexContact = /[+][9][1]\s[7-9][0-9]{4}\s[0-9]{5}/;
 var regexChar = /[a-zA-Z]/;
 var regexCap = /[A-Z]/;
-var regexAtDot = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/g;
+var regexAtDot = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 
 let valid;
 
@@ -19,64 +19,82 @@ function validDetails() {
 
   if (fName == "") {
     alert("name field cannot be empty");
+    document.getElementById("errorfname").style.color = "red";
     return false;
   } else if (regexNotNumAndChar.test(fName)) {
     alert("name can not be a number or special character");
+    document.getElementById("errorfname").style.color = "red";
     return false;
   } else if (fName.length == 1) {
     alert("name can not be a single character");
+    document.getElementById("errorfname").style.color = "red";
     return false;
   }
 
   // /////////////////////////////////////////////////////////////////
   // /*----------------last name validation------------------------*/
-
-  if (lName == "") {
+  else if (lName == "") {
+    document.getElementById("errorfname").style.color = "white";
     alert("last name field cannot be empty");
+    document.getElementById("errorlname").style.color = "red";
     return false;
   } else if (regexNotNumAndChar.test(lName)) {
     alert("last name can not be a number or special character");
+    document.getElementById("errorlname").style.color = "red";
     return false;
   } else if (lName.length == 1) {
     alert("last name can not be a single character");
+    document.getElementById("errorlname").style.color = "red";
     return false;
   }
 
   /////////////////////////////////////////////////////////////////
   /*----------------contact no: validation------------------------*/
-
-  if (contact == "") {
+  else if (contact == "") {
+    document.getElementById("errorlname").style.color = "white";
     alert("Enter Contact no:");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   } else if (contact.length != 15) {
     alert("Please check contact no:");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   } else if (regexChar.test(contact)) {
     alert("Contact no: can not have characters");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   } else if (!regexContact.test(contact)) {
-    alert("invalid");
+    alert("invalid contact no.");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   }
 
   /////////////////////////////////////////////////////////////////
   /*---------------------email validation------------------------*/
   else if (email == "") {
+    document.getElementById("errorcontact").style.color = "white";
     alert("Please enter email");
+    document.getElementById("erroremail").style.color = "red";
     return false;
   } else if (regexCap.test(email)) {
     alert("email don't have capital letters");
+    document.getElementById("erroremail").style.color = "red";
     return false;
   } else if (!regexAtDot.test(email)) {
     alert("Enter valid email");
+    document.getElementById("erroremail").style.color = "red";
     return false;
   }
-
+  document.getElementById("erroremail").style.color = "white";
 }
 
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/*                            json                                 */
+/////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 
-function jsonFile (){
-
+function jsonFile() {
   var fName = document.getElementById("fname").value;
   var lName = document.getElementById("lname").value;
   var contact = document.getElementById("contact").value;
@@ -87,43 +105,51 @@ function jsonFile (){
 
   if (fName == "") {
     alert("name field cannot be empty");
+    document.getElementById("errorfname").style.color = "red";
     return false;
   } else if (regexNotNumAndChar.test(fName)) {
     alert("name can not be a number or special character");
+    document.getElementById("errorfname").style.color = "red";
     return false;
   } else if (fName.length == 1) {
     alert("name can not be a single character");
+    document.getElementById("errorfname").style.color = "red";
     return false;
   }
 
   // /////////////////////////////////////////////////////////////////
   // /*----------------last name validation------------------------*/
-
-  if (lName == "") {
+  else if (lName == "") {
     alert("last name field cannot be empty");
+    document.getElementById("errorlname").style.color = "red";
     return false;
   } else if (regexNotNumAndChar.test(lName)) {
     alert("last name can not be a number or special character");
+    document.getElementById("errorlname").style.color = "red";
     return false;
   } else if (lName.length == 1) {
     alert("last name can not be a single character");
+    document.getElementById("errorlname").style.color = "red";
     return false;
   }
 
   /////////////////////////////////////////////////////////////////
   /*----------------contact no: validation------------------------*/
-
-  if (contact == "") {
+  else if (contact == "") {
     alert("Enter Contact no:");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   } else if (contact.length != 15) {
     alert("Please check contact no:");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   } else if (regexChar.test(contact)) {
     alert("Contact no: can not have characters");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   } else if (!regexContact.test(contact)) {
-    alert("invalid");
+    alert("invalid contact no.");
+    document.getElementById("errorcontact").style.color = "red";
     return false;
   }
 
@@ -131,21 +157,33 @@ function jsonFile (){
   /*---------------------email validation------------------------*/
   else if (email == "") {
     alert("Please enter email");
+    document.getElementById("erroremail").style.color = "red";
     return false;
   } else if (regexCap.test(email)) {
     alert("email don't have capital letters");
+    document.getElementById("erroremail").style.color = "red";
     return false;
   } else if (!regexAtDot.test(email)) {
     alert("Enter valid email");
+    document.getElementById("erroremail").style.color = "red";
     return false;
   }
 
   const userDetails = {
-    "First Name" : document.getElementById("fname").value,
-    "Last Name" : document.getElementById("lname").value,
-    "Contact No:" : document.getElementById("contact").value,
-    "Email" : document.getElementById("email").value
+    "First Name": document.getElementById("fname").value,
+    "Last Name": document.getElementById("lname").value,
+    "Contact No:": document.getElementById("contact").value,
+    Email: document.getElementById("email").value,
   };
 
-  document.getElementById("jsondata").innerHTML = `First Name: ${userDetails["First Name"]} \n Last Name: ${userDetails["Last Name"]}  Contact No.: ${userDetails["Contact No:"]}  Email: ${userDetails.Email}`;
+  document.getElementById("td1").innerHTML = userDetails["First Name"];
+  document.getElementById("td2").innerHTML = userDetails["Last Name"];
+  document.getElementById("td3").innerHTML = userDetails["Contact No:"];
+  document.getElementById("td4").innerHTML = userDetails["Email"];
+  document.getElementById("td11").innerHTML = "First Name:";
+  document.getElementById("td22").innerHTML = "Second Name:";
+  document.getElementById("td33").innerHTML = "Contact No.:";
+  document.getElementById("td44").innerHTML = "Email:";
+
+  document.getElementById("tablecss").style.border = "1px solid black";
 }
