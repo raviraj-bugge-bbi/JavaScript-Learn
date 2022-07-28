a224212c76a4d374c75600c56d3a47552ca457f8
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-07-28 14:40:59 +0530

    completed Assignment html form with validation and json

--- Assignment - HTML form with javascript validation/validation.js
+++ Assignment - HTML form with javascript validation/validation.js
@@ -6,14 +6,13 @@ var regexChar = /[a-zA-Z]/;
 var regexCap = /[A-Z]/;
 var regexAtDot = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/g;
 
-var errorFname = document.getElementsByClassName("errorfname")[0].styl
+let valid;
 
 function validDetails() {
   var fName = document.getElementById("fname").value;
   var lName = document.getElementById("lname").value;
   var contact = document.getElementById("contact").value;
   var email = document.getElementById("email").value;
-
 
   /////////////////////////////////////////////////////////////////
   /*----------------first name validation------------------------*/
@@ -25,7 +24,6 @@ function validDetails() {
     alert("name can not be a number or special character");
     return false;
   } else if (fName.length == 1) {
-
     alert("name can not be a single character");
     return false;
   }
@@ -73,4 +71,81 @@ function validDetails() {
     alert("Enter valid email");
     return false;
   }
+
+}
+
+
+function jsonFile (){
+
+  var fName = document.getElementById("fname").value;
+  var lName = document.getElementById("lname").value;
+  var contact = document.getElementById("contact").value;
+  var email = document.getElementById("email").value;
+
+  /////////////////////////////////////////////////////////////////
+  /*----------------first name validation------------------------*/
+
+  if (fName == "") {
+    alert("name field cannot be empty");
+    return false;
+  } else if (regexNotNumAndChar.test(fName)) {
+    alert("name can not be a number or special character");
+    return false;
+  } else if (fName.length == 1) {
+    alert("name can not be a single character");
+    return false;
+  }
+
+  // /////////////////////////////////////////////////////////////////
+  // /*----------------last name validation------------------------*/
+
+  if (lName == "") {
+    alert("last name field cannot be empty");
+    return false;
+  } else if (regexNotNumAndChar.test(lName)) {
+    alert("last name can not be a number or special character");
+    return false;
+  } else if (lName.length == 1) {
+    alert("last name can not be a single character");
+    return false;
+  }
+
+  /////////////////////////////////////////////////////////////////
+  /*----------------contact no: validation------------------------*/
+
+  if (contact == "") {
+    alert("Enter Contact no:");
+    return false;
+  } else if (contact.length != 15) {
+    alert("Please check contact no:");
+    return false;
+  } else if (regexChar.test(contact)) {
+    alert("Contact no: can not have characters");
+    return false;
+  } else if (!regexContact.test(contact)) {
+    alert("invalid");
+    return false;
+  }
+
+  /////////////////////////////////////////////////////////////////
+  /*---------------------email validation------------------------*/
+  else if (email == "") {
+    alert("Please enter email");
+    return false;
+  } else if (regexCap.test(email)) {
+    alert("email don't have capital letters");
+    return false;
+  } else if (!regexAtDot.test(email)) {
+    alert("Enter valid email");
+    return false;
+  }
+
+  const userDetails = {
+    "First Name" : document.getElementById("fname").value,
+    "Last Name" : document.getElementById("lname").value,
+    "Contact No:" : document.getElementById("contact").value,
+    "Email" : document.getElementById("email").value
+  };
+
+  document.getElementById("jsondata").innerHTML = `First Name: ${userDetails["First Name"]} \n Last Name: ${userDetails["Last Name"]}  Contact No.: ${userDetails["Contact No:"]}  Email: ${userDetails.Email}`;
 }
