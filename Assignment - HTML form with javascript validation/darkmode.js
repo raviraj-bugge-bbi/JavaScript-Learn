75fbf51f0eb33b228a51caabca7599738ed4764e
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-08-04 16:24:46 +0530

    optimization 5 and made changes

---
+++ Assignment - HTML form with javascript validation/darkmode.js
@@ -0,0 +1,20 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', nextDark);
+
+function nextDark(){
+    let returnFlag;
+    if (localStorage.getItem("flag") == 1) {
+        document.getElementById("next").style.backgroundColor = "black";
+        document.getElementById("next").style.color = "white";
+        
+        return(returnFlag = localStorage.setItem('flag', '1'));
+    } else {
+        document.getElementById("next").style.backgroundColor = "white";
+        document.getElementById("next").style.color = "black";
+
+        return(returnFlag =localStorage.setItem('flag', '0'));
+    }
+}
+
+
--- Assignment - HTML form with javascript validation/validation.js
+++ Assignment - HTML form with javascript validation/validation.js
@@ -11,82 +11,82 @@ var regexAtDot = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 /*                             Form Validation                         */
 /////////////////////////////////////////////////////////////////////////
 
-function validDetails() {
-  var fName = document.getElementById("fname").value;
-  var lName = document.getElementById("lname").value;
-  var contact = document.getElementById("contact").value;
-  var email = document.getElementById("email").value;
+// function validDetails() {
+//   var fName = document.getElementById("fname").value;
+//   var lName = document.getElementById("lname").value;
+//   var contact = document.getElementById("contact").value;
+//   var email = document.getElementById("email").value;
 
-  /*----------------first & last name validation------------------------*/
-  if (fName == "" && lName == "") {
-    alert("name field cannot be empty");
-    document.getElementById("errorfname").style.color = "red";
-    document.getElementById("errorfname").style.visibility = "visible";
-    document.getElementById("errorlname").style.color = "red";
-    document.getElementById("errorlname").style.visibility = "visible";
-    return false;
-  } else if (regexNotNumAndChar.test(fName) && regexNotNumAndChar.test(lName)) {
-    alert("name can not be a number or special character");
-    document.getElementById("errorfname").style.color = "red";
-    document.getElementById("errorfname").style.visibility = "visible";
-    document.getElementById("errorlname").style.color = "red";
-    document.getElementById("errorlname").style.visibility = "visible";
-    return false;
-  } else if (fName.length == 1 && lName.length == 1) {
-    alert("name can not be a single character");
-    document.getElementById("errorfname").style.color = "red";
-    document.getElementById("errorfname").style.visibility = "visible";
-    document.getElementById("errorlname").style.color = "red";
-    document.getElementById("errorlname").style.visibility = "visible";
-    return false;
-  } else if (contact == "") {
-    /*----------------contact no: validation------------------------*/
-    document.getElementById("errorfname").style.color = "white";
-    document.getElementById("errorfname").style.visibility = "hidden";
-    document.getElementById("errorlname").style.color = "white";
-    document.getElementById("errorlname").style.visibility = "hidden";
-    alert("Enter Contact no:");
-    document.getElementById("errorcontact").style.color = "red";
-    document.getElementById("errorcontact").style.visibility = "visible";
-    return false;
-  } else if (contact.length != 15) {
-    alert("Please check contact no:");
-    document.getElementById("errorcontact").style.color = "red";
-    document.getElementById("errorcontact").style.visibility = "visible";
-    return false;
-  } else if (regexChar.test(contact)) {
-    alert("Contact no: can not have characters");
-    document.getElementById("errorcontact").style.color = "red";
-    document.getElementById("errorcontact").style.visibility = "visible";
-    return false;
-  } else if (!regexContact.test(contact)) {
-    alert("invalid contact no.");
-    document.getElementById("errorcontact").style.color = "red";
-    document.getElementById("errorcontact").style.visibility = "visible";
-    return false;
-  } else if (email == "") {
-    /*---------------------email validation------------------------*/
-    document.getElementById("errorcontact").style.color = "white";
-    document.getElementById("errorcontact").style.visibility = "hidden";
-    alert("Please enter email");
-    document.getElementById("erroremail").style.color = "red";
-    document.getElementById("erroremail").style.visibility = "visible";
-    return false;
-  } else if (regexCap.test(email)) {
-    alert("email don't have capital letters");
-    document.getElementById("erroremail").style.color = "red";
-    document.getElementById("erroremail").style.visibility = "visible";
-    return false;
-  } else if (!regexAtDot.test(email)) {
-    alert("Enter valid email");
-    document.getElementById("erroremail").style.color = "red";
-    document.getElementById("erroremail").style.visibility = "visible";
-    return false;
-  }
-  document.getElementById("erroremail").style.color = "white";
-  document.getElementById("erroremail").style.visibility = "hidden";
-  return true;
-}
+//   /*----------------first & last name validation------------------------*/
+//   if (fName == "" && lName == "") {
+//     alert("name field cannot be empty");
+//     document.getElementById("errorfname").style.color = "red";
+//     document.getElementById("errorfname").style.visibility = "visible";
+//     document.getElementById("errorlname").style.color = "red";
+//     document.getElementById("errorlname").style.visibility = "visible";
+//     return false;
+//   } else if (regexNotNumAndChar.test(fName) && regexNotNumAndChar.test(lName)) {
+//     alert("name can not be a number or special character");
+//     document.getElementById("errorfname").style.color = "red";
+//     document.getElementById("errorfname").style.visibility = "visible";
+//     document.getElementById("errorlname").style.color = "red";
+//     document.getElementById("errorlname").style.visibility = "visible";
+//     return false;
+//   } else if (fName.length == 1 && lName.length == 1) {
+//     alert("name can not be a single character");
+//     document.getElementById("errorfname").style.color = "red";
+//     document.getElementById("errorfname").style.visibility = "visible";
+//     document.getElementById("errorlname").style.color = "red";
+//     document.getElementById("errorlname").style.visibility = "visible";
+//     return false;
+//   } else if (contact == "") {
+//     /*----------------contact no: validation------------------------*/
+//     document.getElementById("errorfname").style.color = "white";
+//     document.getElementById("errorfname").style.visibility = "hidden";
+//     document.getElementById("errorlname").style.color = "white";
+//     document.getElementById("errorlname").style.visibility = "hidden";
+//     alert("Enter Contact no:");
+//     document.getElementById("errorcontact").style.color = "red";
+//     document.getElementById("errorcontact").style.visibility = "visible";
+//     return false;
+//   } else if (contact.length != 15) {
+//     alert("Please check contact no:");
+//     document.getElementById("errorcontact").style.color = "red";
+//     document.getElementById("errorcontact").style.visibility = "visible";
+//     return false;
+//   } else if (regexChar.test(contact)) {
+//     alert("Contact no: can not have characters");
+//     document.getElementById("errorcontact").style.color = "red";
+//     document.getElementById("errorcontact").style.visibility = "visible";
+//     return false;
+//   } else if (!regexContact.test(contact)) {
+//     alert("invalid contact no.");
+//     document.getElementById("errorcontact").style.color = "red";
+//     document.getElementById("errorcontact").style.visibility = "visible";
+//     return false;
+//   } else if (email == "") {
+//     /*---------------------email validation------------------------*/
+//     document.getElementById("errorcontact").style.color = "white";
+//     document.getElementById("errorcontact").style.visibility = "hidden";
+//     alert("Please enter email");
+//     document.getElementById("erroremail").style.color = "red";
+//     document.getElementById("erroremail").style.visibility = "visible";
+//     return false;
+//   } else if (regexCap.test(email)) {
+//     alert("email don't have capital letters");
+//     document.getElementById("erroremail").style.color = "red";
+//     document.getElementById("erroremail").style.visibility = "visible";
+//     return false;
+//   } else if (!regexAtDot.test(email)) {
+//     alert("Enter valid email");
+//     document.getElementById("erroremail").style.color = "red";
+//     document.getElementById("erroremail").style.visibility = "visible";
+//     return false;
+//   }
+//   document.getElementById("erroremail").style.color = "white";
+//   document.getElementById("erroremail").style.visibility = "hidden";
+//   return true;
+// }
 
 /////////////////////////////////////////////////////////////////////
 /*                            JSON                                 */
@@ -112,22 +112,23 @@ function jsonFile() {
 /*                        DARK MODE TOGGLE                       */
 ///////////////////////////////////////////////////////////////////
 
-sessionStorage.setItem('flag','0');
+
 
 function darkMode() {
-  let flag = sessionStorage.getItem('flag');
+  let flag = localStorage.getItem('flag');
   if(flag == 0){
     document.getElementById("body").style.backgroundColor = "black";
     document.getElementById("body").style.color = "white";
     document.getElementById("dbtn").value = "Light Mode";
-    sessionStorage.setItem('flag','1');
+    localStorage.setItem('flag','1');
   }else{
     document.getElementById("body").style.backgroundColor = "white";
     document.getElementById("body").style.color = "black";
     document.getElementById("dbtn").value = "Dark Mode";
-    sessionStorage.setItem('flag','0');
+    localStorage.setItem('flag','0');
   }
 }
+
 
 ///////////////////////////////////////////////////////////////////
 /*                        CLEAR JSON VIEW                        */
