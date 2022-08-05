915370ec38a39686f00836652400ea44d00152f7
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-08-05 10:10:52 +0530

    optimization 5 and made changes

---
+++ Assignment - HTML form with javascript validation/prime.js
@@ -0,0 +1,42 @@
+let flag = 0;
+
+for (let a = 2; a <= 20; a++) {
+    for (let b = 2; b <= a; b++) {
+        if (a % b == 0) {
+            flag += 1;
+        }
+    }
+
+    if (flag == 1) {
+        console.log(a);
+    }
+    flag = 0;
+}
+
+
+// if(flag > 1){
+//     console.log("not prime");
+// }
+// else{
+//     console.log("prime");
+// }
+
+// let n = 30;
+
+// function ifPrime(n) {
+
+//     let flag = 0;
+
+//     for (let a = 2; a <= n; a++) {
+//         for (let b = 2; b <= a; b++) {
+//             if (a % b == 0) {
+//                 flag += 1;
+//             }
+//         }
+
+//         if (flag == 1) {
+//             console.log(a);
+//         }
+//         flag = 0;
+//     }
+// }
