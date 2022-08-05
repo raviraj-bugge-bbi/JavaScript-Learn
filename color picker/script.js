60d3607021373568afe864317ca9cdf848ea6729
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-08-05 16:02:07 +0530

    Project: Color Picker

---
+++ color picker/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded',placeColor);
+
+function placeColor(){
+    document.getElementById("Empty-box").addEventListener('click',fillColor);
+
+    function fillColor(e){
+        if(e.target.className === 'box'){
+            e.target.style.backgroundColor = document.getElementById("color-picker-btn").value;
+        }
+    }
+}
+
+function darkMode() {
+  let flag = localStorage.getItem('flag');
+  if (flag == 0) {
+    document.getElementById("main-body").style.backgroundColor = "#38444D";
+    document.getElementById("main-body").style.color = "#DDDDDD";
+    document.getElementById("dbtn").value = "Light Mode";
+    localStorage.setItem('flag', '1');
+  } else {
+    document.getElementById("main-body").style.backgroundColor = "#E7E9EB";
+    document.getElementById("main-body").style.color = "#DDDDDD";
+    document.getElementById("dbtn").value = "Dark Mode";
+    localStorage.setItem('flag', '0');
+  }
+}
