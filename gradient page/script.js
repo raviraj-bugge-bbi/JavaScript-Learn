a4b46ed64d29d03547669b0b12ff1e36416b5f0a
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-08-08 17:44:27 +0530

    Project: Gradient Maker - DONE

--- gradient page/script.js
+++ gradient page/script.js
@@ -1,8 +1,10 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', select);
 function select() {
-    let colorOne = document.getElementById("firstColor").value;
-    let colorTwo = document.getElementById("secondColor").value;
 
-    document.getElementById("body").style.backgroundImage = "linear-gradient(to right"+","+ colorOne +","+ colorTwo+")";
+    document.getElementById("apply").addEventListener('click', applyColor);
+
+    function applyColor() {
+        document.getElementById("body").style.backgroundImage = "linear-gradient(to right," + firstColor.value + "," + secondColor.value + ")";
+    }
 }
