973ad956b763d81ade241f9264dd7b0687617492
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-08-11 12:31:02 +0530

    Section 8: Data Structure

---
+++ Developer Skills & Editor Setup/Coding Challenge 1.js
@@ -0,0 +1,15 @@
+`use strict`;
+
+const Data1 = [17, 21, 23];
+const Data2 = [12,5,-5,0,4];
+
+function printForecast(arr){
+    let str = '';
+    for(let i = 0;i<arr.length;i++){
+        str += `${arr[i]}°C in ${i+1} days ... `
+    }
+    console.log(str);
+}
+
+printForecast(Data1);
+printForecast(Data2);
